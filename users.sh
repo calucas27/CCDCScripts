bb9beec317159a8c40349313e732ed81bb8d3faa
calucas27 <chaselucas265@gmail.com>
2017-10-12 16:42:30 -0500

    Updated users.sh
    Changed the script to add users first, and instead of locking just user accounts, covers all accounts (including system accounts) and drops them into /bin/false.

--- users.sh
+++ users.sh
@@ -3,19 +3,19 @@
 #Short script to lock down user accounts, except for the root account.
 #Run with sudo or as root.
 
-users="$(awk -F: '$7 == "/bin/bash" { print $1 }' /etc/passwd | grep -v root)"
-
-#lock all user accounts except for root
-echo -e "Locking user accounts!"
-for user in $users
-do
-	passwd -l $user
-done
-
 #add new user account and group them
 echo -e "Adding backup admin account!"
 useradd ccdcadmin
 usermod -aG sudo ccdcadmin
+
+users="$(cut -d : -f 1 /etc/passwd | grep -v root | grep -v ccdcadmin)"
+
+#deny login for all accounts except for root and ccdcadmin
+echo -e "Locking user accounts!"
+for user in $users
+do
+    usermod -s /bin/false $user
+done
 
 #purge ssh keys
 rm -rf $HOME/.ssh/authorized_keys
